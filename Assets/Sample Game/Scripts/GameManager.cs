bf5512855da01a5f31199c6b5e24bdaab602d28e
YIFN <lideram@naver.com>
2024-07-31 11:02:12 +0900

    재화 시스템 및 클릭 추가

--- Assets/Sample Game/Scripts/GameManager.cs
+++ Assets/Sample Game/Scripts/GameManager.cs
@@ -6,4 +6,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAc;
+
+    public void ChangeAC(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = LevelAc[level-1];
+
+    }
 }
